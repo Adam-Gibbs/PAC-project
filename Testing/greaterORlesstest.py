ba3210008a82a8b0ae175a00d4fe1bf96b30b3b4
Adam G <adam-gibbs@outlook.com>
2018-10-26 10:31:37 +0100

    mabe

---
+++ Testing/greaterORlesstest.py
@@ -0,0 +1,14 @@
+if "a" < "b":
+    print ("True")
+else:
+    print("False")
+
+if 1 < 2:
+    print ("True")
+else:
+    print("False")
+
+if 1 <= 2:
+    print ("True")
+else:
+    print("False")
---
+++ Testing/lentest.py
@@ -0,0 +1,3 @@
+dave = ["a","b","e","g","h","i","j","k"]
+print(len(dave))
+print(dave[3-8])
