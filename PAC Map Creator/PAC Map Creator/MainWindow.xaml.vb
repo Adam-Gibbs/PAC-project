32329fd22f4bca47c8f1547c6683e360b603cf42
Adam G <adam-gibbs@outlook.com>
2018-10-26 12:38:13 +0100

    Added Design Form

--- PAC Map Creator/PAC Map Creator/MainWindow.xaml.vb
+++ PAC Map Creator/PAC Map Creator/MainWindow.xaml.vb
@@ -1,5 +1,10 @@
 ﻿Class MainWindow
-    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+    Public Temp As String = "One"
 
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        Dim DesignForm As Design_Window
+        DesignForm = New Design_Window()
+        DesignForm.Show()
+        MsgBox(Temp)
     End Sub
 End Class
