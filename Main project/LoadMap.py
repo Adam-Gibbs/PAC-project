bb59bccab4f5512e7085b58d8de39bc9f36b47fc
Adam G <adam-gibbs@outlook.com>
2018-10-30 07:33:13 +0000

    Creates map but imputs string not int

--- Main project/LoadMap.py
+++ Main project/LoadMap.py
@@ -1,9 +1,14 @@
 from MapStruct import Map
+import os
 
 # load map file from here, reading each line, interpreting it to MapSturct
 
 ReadData = list()
+FileName = "Maps\Test.txt"
 
-with open("Maps\Test.txt") as infile:       # Context manager, automatically closes file
+with open(FileName) as infile:       # Context manager, automatically closes file
     for line in infile:
         ReadData.append(line.strip("\n"))
+
+print(os.path.basename(FileName)[:-4])
+CurrMap = Map(ReadData[0].split(","),os.path.basename(FileName),ReadData[1])
