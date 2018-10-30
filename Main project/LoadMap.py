e55ba9128c188c9900228132a9f809e8ff7b3e9a
Adam G <adam-gibbs@outlook.com>
2018-10-30 07:43:23 +0000

    Continuation of loading file

--- Main project/LoadMap.py
+++ Main project/LoadMap.py
@@ -3,12 +3,21 @@ import os
 
 # load map file from here, reading each line, interpreting it to MapSturct
 
-ReadData = list()
-FileName = "Maps\Test.txt"
+def LoadMap(FileName):
+    ReadData = list()
 
-with open(FileName) as infile:       # Context manager, automatically closes file
-    for line in infile:
-        ReadData.append(line.strip("\n"))
+    # Load in File to memory
+    with open(FileName) as infile:       # Context manager, automatically closes file
+        for line in infile:
+            ReadData.append(line.strip("\n"))
 
-print(os.path.basename(FileName)[:-4])
-CurrMap = Map(ReadData[0].split(","),os.path.basename(FileName),ReadData[1])
+    # Load in MetaData
+    CurrMap = Map(list(map(int, ReadData[0].split(","))), os.path.basename(FileName)[:-4], ReadData[1]) # List(map, converts list of strings to ints
+                                                                                                        # Could use a list comprehension here instead
+    # Now load in squares
+    while loop = True:
+        
+
+    return CurrMap
+
+LoadMap("Maps\Test.txt")
