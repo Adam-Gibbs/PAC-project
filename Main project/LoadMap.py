b2f4c2f44c6b83143c36d50810f1169c69582ca7
Adam-LAPTOP <adam-gibbs@outlook.com>
2018-11-01 11:57:21 +0000

    fin load map, but does not work

--- Main project/LoadMap.py
+++ Main project/LoadMap.py
@@ -3,7 +3,7 @@ import os
 
 # load map file from here, reading each line, interpreting it to MapSturct
 
-def LoadMap(FileName):
+def LoadMap(FileName, ScreenSize):
     ReadData = list()
 
     # Load in File to memory
@@ -11,16 +11,22 @@ def LoadMap(FileName):
         for line in infile:
             ReadData.append(line.strip("\n"))
 
+    Size = list(map(int, ReadData[0].split(","))) # List(map, converts list of strings to ints. Could use a list comprehension here instead
+
     # Load in MetaData
-    CurrMap = Map(list(map(int, ReadData[0].split(","))), os.path.basename(FileName)[:-4], ReadData[1]) # List(map, converts list of strings to ints
-                                                                                                        # Could use a list comprehension here instead
+    CurrMap = Map(Size, os.path.basename(FileName)[:-4], ReadData[1]) 
+
     # Now load in squares
     ReadData = ReadData[2].split("/")
     TempData = list()
-    for loop in range (len(ReadData))
-        TempData = ReadData.split(",")
+
+    SquareSize = [(ScreenSize[0]-40)/Size[0], (ScreenSize[1]-40)/Size[1]]
+
+    for loop in range (len(ReadData)):
+        TempData = ReadData[loop].split(",")
+        Row = loop // Size[0]
+        Column = loop % Size[0]
         
+        CurrMap.InputSquare([20+(Column*SquareSize[0]), 20+(Column*SquareSize[1])], [20+SquareSize[0]+(Column*SquareSize[0]), 20+SquareSize[1]+(Column*SquareSize[1])], [TempData[0],TempData[1],TempData[2],TempData[3]],TempData[4], [Column,Row]) 
 
     return CurrMap
-
-LoadMap("Maps\Test.txt")
--- Main project/Main.py
+++ Main project/Main.py
@@ -1,23 +1,12 @@
 import pygame, sys
 from pygame.locals import *
-from MapStruct import Map
+from MapStruct import *
+from LoadMap import *
 
-CurrMap = Map([4,3],"TestMap","Adam")
+pygame.init()
+CurrMap = LoadMap("Maps\Test.txt", [1000, 640])
 clock = pygame.time.Clock()
 ExitBool = False
-
-CurrMap.InputSquare([20,20],[260,220],[True,False,False,True],"E",[0,0]) 
-CurrMap.InputSquare([260,20],[500,220],[True,True,False,False],"E",[1,0])
-CurrMap.InputSquare([500,20],[740,220],[True,False,False,True],"P",[2,0])
-CurrMap.InputSquare([740,20],[980,220],[True,True,False,False],"E",[3,0])
-CurrMap.InputSquare([20,220],[260,420],[False,False,False,True],"E",[0,1])
-CurrMap.InputSquare([260,220],[500,420],[False,False,True,False],"S",[1,1])
-CurrMap.InputSquare([500,220],[740,420],[False,False,False,False],"S",[2,1])
-CurrMap.InputSquare([740,220],[980,420],[False,True,False,False],"E",[3,1])
-CurrMap.InputSquare([20,420],[260,620],[False,False,True,True],"E",[0,2])
-CurrMap.InputSquare([260,420],[500,620],[False,True,True,False],"G",[1,2])
-CurrMap.InputSquare([500,420],[740,620],[False,False,True,True],"E",[2,2])
-CurrMap.InputSquare([740,420],[980,620],[False,True,True,False],"U",[3,2])
 
 StartDisplay = pygame.display.set_mode((1000, 640))
 White=(255,255,255)
@@ -32,6 +21,7 @@ StartDisplay.fill(Black)
 for Row in range(CurrMap.GiveSize("X")):
     for Column in range(CurrMap.GiveSize("Y")):
         temp = CurrMap.GiveSquare([Row,Column]).GiveWalls()
+        print(temp)
         for wall in range (0,4):
             # Remember that rect = [TopLeft([X,Y]), width, height]
             pygame.draw.rect(StartDisplay, CList[1], (temp[wall][0][0], temp[wall][0][1], temp[wall][1][0], temp[wall][1][1]), 10)
