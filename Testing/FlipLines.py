4ea8708506c9159b6eb6e1cd306d5706019b3890
Adam-LAPTOP <adam-gibbs@outlook.com>
2018-12-09 12:32:21 +0000

    Fixed fliplines so now flipes walls on X axis

--- Testing/FlipLines.py
+++ Testing/FlipLines.py
@@ -6,5 +6,19 @@ while line != "":
     if line != "":
         lines.append(line)
 
-for loop in range(len(lines)-1,0,-1):
+nlines = list()
+
+for line in lines:
+    group = line.split("/")
+    ngroup = ""
+    for index,items in enumerate(group):
+        items = (items[4] + "," + items[2] + "," + items[0] + "," + items[6] + "," + items[8])
+        if index != 0:
+            ngroup += "/"+items
+        else:
+            ngroup+= items
+    nlines.append(ngroup)
+lines = nlines
+    
+for loop in range(len(lines)-1,-1,-1):
     print(lines[loop])
