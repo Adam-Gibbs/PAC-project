18709643bbc42348a318d61f78c9d2b01ee4fa21
Adam G <adam-gibbs@outlook.com>
2018-12-12 07:57:02 +0000

    Fixed fliplines

--- Testing/FlipLines.py
+++ Testing/FlipLines.py
@@ -1,10 +1,19 @@
 lines = list()
 line = "temp"
+First = ""
 
 while line != "":
     line = input("Add lines (RETURN for finished)")
     if line != "":
         lines.append(line)
 
-for loop in range(len(lines)-1,0,-1):
+for line in lines:
+    for i,cha in enumerate(line):
+        if i / 10 == 0:
+            First = cha
+        elif (i-4) / 10 == 0:
+            line[i-4] = cha
+            cha = First        
+
+for loop in range(len(lines)-1,-1,-1):
     print(lines[loop])
