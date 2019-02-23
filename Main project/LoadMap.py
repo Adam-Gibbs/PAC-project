66ddd642e610a8b0c7d688c9178bcf7c192c1bed
Adam G <adam-gibbs@outlook.com>
2019-02-23 17:43:32 +0000

    Commented LoadMap file

--- Main project/LoadMap.py
+++ Main project/LoadMap.py
@@ -4,6 +4,7 @@ import os
 # load map file from here, reading each line, interpreting it to MapSturct.
 
 
+# This loads in the map file
 def LoadMap(FileName, ScreenSize):
     ReadData = list()
     ScreenSize = [ScreenSize[0]-200, ScreenSize[1]]
@@ -28,11 +29,13 @@ def LoadMap(FileName, ScreenSize):
 
     SquareSize = [(ScreenSize[0]-40)/Size[0], (ScreenSize[1]-40)/Size[1]]
 
+    # This loads and splits the wall data for each square
     for index, data in enumerate(ReadData):
         TempData = data.split(",")
         Row = index // Size[0]
         Column = index % Size[0]
 
+        # This creates a square object with this data, saved into map object
         CurrMap.InputSquare([220+((Column)*SquareSize[0]),
                              20+(Row*SquareSize[1])],
                             [220+SquareSize[0]+((Column)*SquareSize[0]),
@@ -40,4 +43,5 @@ def LoadMap(FileName, ScreenSize):
                             [TempData[0], TempData[1], TempData[2],
                              TempData[3]], TempData[4], [Column, Row])
 
+    # Map object is returned
     return CurrMap
